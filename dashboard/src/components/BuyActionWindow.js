5193b0996cb73d389fa3f29409f846cb592f03a5
yuvraj singh <yuvipaji222002@gmail.com>
2025-07-16 16:23:47 +0530

    add new files

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://zerodha-clone-12.onrender.com/newOrder", {
+    axios.post("https://zerodha-clone-13.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
